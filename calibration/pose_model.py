2c791dd81fd68de3f125b71ac0769bbbc6cda297
haebeom <zang09@naver.com>
2025-10-20 03:18:20 +0900

    fix pose error

--- calibration/pose_model.py
+++ calibration/pose_model.py
@@ -7,7 +7,7 @@ class Pose(torch.nn.Module):
     """
     extrinsic parameters
     """
-    def __init__(self, opt, ext):
+    def __init__(self, opt, ext, init=None):
         super().__init__()
         # --- extrinsic parameters requiring gradient--- 
          
@@ -17,17 +17,19 @@ class Pose(torch.nn.Module):
         torch.nn.init.zeros_(self.R.weight)
 
         # --- initial value ---
-        self.correct = ext  #(6)
+        self.correct = ext  # 6vectors
 
-        error = torch.rand(6, dtype=torch.float, device="cuda")
-        error /= torch.linalg.norm(error)
-
-        self.init = self.correct.se3()
-        # near
-        # self.init[3:] += 0.1
-
-        # far
-        self.init = self.correct.se3() + 0.2
+        if opt.config.init_method == "from_lidar":
+            if init is None:
+                raise ValueError("init must be provided for from_lidar initialization")
+            self.init = SE3(se3=init.se3())
+        elif opt.config.init_method == "near":
+            self.init = SE3(se3=torch.tensor([0, 0, 0, 1, 1, 1]).cuda().float() * 0.1) @ self.correct # [R, t]
+        elif opt.config.init_method == "far":
+            self.init = SE3(se3=torch.ones(6).cuda().float() * 0.2) @ self.correct
+        else:
+            raise ValueError("Unknown initialization method")
+        
         self.change = []
 
 
@@ -39,8 +41,8 @@ class Pose(torch.nn.Module):
 
     def forward(self) -> torch.Tensor:
         ext = torch.cat((self.R.weight[0], self.t.weight[0]))
-        return SE3(se3=ext + self.init) # [3, 4]
-    
+        return SE3(se3=ext) @ self.init
+
     @torch.no_grad()
     def change_rate(self):
         ext = self.forward()
@@ -49,19 +51,13 @@ class Pose(torch.nn.Module):
         self.change.append([t, rot])
         if len(self.change) > 500:
             self.change.pop(0)
-
-        position_change = np.linalg.norm((self.change[-1][0] - self.change[0][0]) / len(self.change))
-        rotation_change = np.rad2deg(
-            np.linalg.norm(
-            Rotation.from_matrix(self.change[-1][1].T @ self.change[0][1]).as_rotvec()
-            )
-        )
-        return [position_change, rotation_change]
-    
+        return [np.linalg.norm((self.change[-1][0] - self.change[0][0]) / len(self.change)), 
+                np.rad2deg(np.linalg.norm(Rotation.from_matrix(self.change[-1][1].transpose() @ self.change[0][1]).as_rotvec()))
+                ]
     @property
     @torch.no_grad()
     def get_error(self):
-        error = self.correct.invert() @ self.forward()
+        error = self.forward() @ self.correct.invert()
         error_R = error.R.cpu().numpy()
         error_t = error.t.cpu().numpy()
         return Rotation.from_matrix(error_R).as_rotvec(degrees=True), error_t
